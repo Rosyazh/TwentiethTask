3d0b0dc0e0fc2fe35f105656da081b4988be4db3
Evgeniy Rossol <rossol.zheka@gmail.com>
2017-03-26 22:52:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+/*
+Practice:
+Develop a Shop class for ordering and purchasing stuff. Develop Order, Customer and Product classes to implement this interaction.
+*/
+
 namespace TwentiethTask
 {
     public class Shop
